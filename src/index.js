cc98f3d069985407968ea4f9029ef62213561dba
TristanLeech <241365@virtualwindow.co.za>
2025-11-05 09:40:50 +0200

    Post Deployment Changes

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import App from './app.js';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+  useEffect(() => {
+           const tagManagerArgs = {
+               gtmId: 'G-82FK57EZVE'
+           };
+           TagManager.initialize(tagManagerArgs);
+       }, []);
 root.render(
   <React.StrictMode>
     <App />
